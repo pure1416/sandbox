20c9d15ac7c61d9c8f453ac360b1c099969ca539
hiromu <kh.534061@gmail.com>
2020-03-27 23:17:57 +0900

    かけらα版（押す、流れる、埋まる）
    とりあえずプレイヤーの中砂がない時に埋まっていきます

--- Assets/Script/かけら/Fragment.cs
+++ Assets/Script/かけら/Fragment.cs
@@ -12,10 +12,15 @@ public class Fragment : MonoBehaviour
     public GameObject playercontroler;
 
     // 変数宣言
-    Vector3 FragmentDir;           // かけらの方向
-    float FragmentSp;              // かけらの速度
-    bool PSandEnpflg;              // プレイヤーの中砂の有無
+    //Vector3 FragmentDir;           // かけらの方向
+    //float FragmentSp;              // かけらの速度
+    bool P_SandEnpflg;              // プレイヤーの中砂の有無
     bool SandCol;                  // 流砂に当たっているかどうか
+
+    float SinkTime;                 // 沈む時間
+    int SinkCount;                  // 何秒かけて沈むか？
+    Vector3 B_Col_Center;           // BoxColliderのCenter
+    Vector3 B_Col_Size;             // BoxColliderのSize
 
     // 当たり判定
     BoxCollider FtCol;
@@ -29,12 +34,17 @@ public class Fragment : MonoBehaviour
         // 初期化
         playercontroler = GameObject.FindGameObjectWithTag("Player");
 
-        FragmentDir = new Vector3(0.0f,0.0f,0.0f);
+        //FragmentDir = new Vector3(0.0f,0.0f,0.0f);
+        //FragmentSp = 5.0f;
+
         SandMoveFtSp = new Vector3(0.0f, 0.0f, 0.0f);
-        FragmentSp = 0.0f;
         SandCol = false;
 
-        PSandEnpflg = playercontroler.GetComponent<PlayerControler>().GetPlayerEnpty(); ;
+        SinkTime = 0.0f;
+        SinkCount = 0;
+        B_Col_Center = new Vector3(0.0f,0.0f,0.0f);
+        B_Col_Size = new Vector3(1.0f, 1.0f, 1.0f);
+        P_SandEnpflg = playercontroler.GetComponent<PlayerControler>().GetPlayerEnpty(); ;
 
         FtCol = GetComponent<BoxCollider>();
         rb = this.GetComponent<Rigidbody>();
@@ -44,88 +54,112 @@ public class Fragment : MonoBehaviour
     void Update()
     {
         // プレイヤーの中砂の有無を常にもってくる
-        PSandEnpflg = playercontroler.GetComponent<PlayerControler>().GetPlayerEnpty();
+        P_SandEnpflg = playercontroler.GetComponent<PlayerControler>().GetPlayerEnpty();
 
-        // 流砂に乗っていたら動く
-        if (SandCol == true)
+        // 大体10秒かけて半分埋まりきるイメージ？
+        if (SinkCount < 100)
         {
-            this.transform.Translate(SandMoveFtSp);
+            SinkTime += Time.deltaTime;
+        }
+
+        // プレイヤーの中砂がないとき徐々に埋まっていく(BoxColliderの位置と大きさを変えてます)
+        if (P_SandEnpflg == true)
+        {
+            if(SinkTime >= 0.1f)
+            {
+                SinkTime = 0.0f;
+                SinkCount += 1;
+                B_Col_Center.y += 0.0025f;
+                B_Col_Size.y -= 0.005f;
+            }
         }
     }
 
-    private void OnCollisionStay(Collision collision)
-    {
-        // プレイヤーが押している方向に移動
-        if (collision.gameObject.tag == "Player")
-        {
-            // プレイヤーの移動方向を入れる
-            FragmentDir = playercontroler.GetComponent<PlayerControler>().GetPlayerDir();
-            // とりあえずプレイヤーと同じにしておく
-            // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
-             rb.velocity = FragmentDir * FragmentSp + new Vector3(0, rb.velocity.y, 0) + SandMoveFtSp;
-        }
 
-        // 流砂の上にいるときに流砂の移動力を受け取る
-        //if (collision.gameObject.tag == "QuickSand")
-        //{
-        //    SandCol = true;
-        //    SandMoveFtSp = collision.gameObject.GetComponent<Quicksand>().GetSandMove();
-        //    SandMoveFtSp /= 100;
-            
-        //    // 流砂がｙ方向に力がかかっていたら重力を切る
-        //    if(SandMoveFtSp.y > 0.0f)
-        //    {
-        //        this.GetComponent<Rigidbody>().useGravity = false;
-        //    }
-        //    Debug.Log(SandMoveFtSp);
+    //private void OnCollisionStay(Collision collision)
+    //{
+    //    // プレイヤーが押している方向に移動
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        PlayerCol = true;
+    //        // プレイヤーの移動方向を入れる
+    //        FragmentDir = playercontroler.GetComponent<PlayerControler>().GetPlayerDir();
+    //        // とりあえずプレイヤーと同じにしておく
+    //        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+    //        if (PlayerCol == true)
+    //        {
+    //            rb.velocity = FragmentDir * FragmentSp + new Vector3(0, rb.velocity.y, 0) + SandMoveFtSp;
 
-        //    // プレイヤーの中砂が落ちきっていたら埋まる処理（徐々に埋まっていく感じに変更予定）
-        //    if (PSandEnpflg == true)
-        //    {
-        //        //FtCol.center = new Vector3(0.0f,0.25f,0.0f);
-        //        //FtCol.size = new Vector3(1.0f,0.5f,1.0f);
-        //    }
-        //}
-    }
+    //        }
+    //        //FragmentMove = FragmentDir * FragmentSp;
+    //        //this.transform.Translate(FragmentMove);
+    //    }
 
-    //流砂から離れるときに流砂の影響を消す
-    private void OnCollisionExit(Collision collision)
-    {
-        //if (collision.gameObject.tag == "QuickSand")
-        //{
-        //    this.GetComponent<Rigidbody>().useGravity = true;
-        //    SandCol = false;
-        //    SandMoveFtSp = new Vector3(0.0f, 0.0f, 0.0f);
-        //}
-    }
+    //    // 流砂の上にいるときに流砂の移動力を受け取る
+    //    //if (collision.gameObject.tag == "QuickSand")
+    //    //{
+    //    //    SandCol = true;
+    //    //    SandMoveFtSp = collision.gameObject.GetComponent<Quicksand>().GetSandMove();
+    //    //    SandMoveFtSp /= 100;
+
+    //    //    // 流砂がｙ方向に力がかかっていたら重力を切る
+    //    //    if(SandMoveFtSp.y > 0.0f)
+    //    //    {
+    //    //        this.GetComponent<Rigidbody>().useGravity = false;
+    //    //    }
+    //    //    Debug.Log(SandMoveFtSp);
+
+    //    //    // プレイヤーの中砂が落ちきっていたら埋まる処理（徐々に埋まっていく感じに変更予定）
+    //    //    if (PSandEnpflg == true)
+    //    //    {
+    //    //        //FtCol.center = new Vector3(0.0f,0.25f,0.0f);
+    //    //        //FtCol.size = new Vector3(1.0f,0.5f,1.0f);
+    //    //    }
+    //    //}
+    //}
+
+    ////流砂から離れるときに流砂の影響を消す
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    //if (collision.gameObject.tag == "QuickSand")
+    //    //{
+    //    //    this.GetComponent<Rigidbody>().useGravity = true;
+    //    //    SandCol = false;
+    //    //    SandMoveFtSp = new Vector3(0.0f, 0.0f, 0.0f);
+    //    //}
+    //}
 
     private void OnTriggerStay(Collider other)
     {
         // 流砂の上にいるときに流砂の移動力を受け取る
         if (other.gameObject.tag == "QuickSand_B")
         {
-            SandCol = true;
             SandMoveFtSp = other.gameObject.GetComponent<Quicksand>().GetSandMove();
-            SandMoveFtSp /= 100;
+            SandMoveFtSp /= 10;
+            this.transform.Translate(SandMoveFtSp);
 
+            // 流砂がｙ方向に力がかかっていなければ重力を付ける
+            if (SandMoveFtSp.y == 0.0f)
+            {
+                this.GetComponent<Rigidbody>().useGravity = true;
+            }
             // 流砂がｙ方向に力がかかっていたら重力を切る
-            if (SandMoveFtSp.y > 0.0f)
+            else
             {
                 this.GetComponent<Rigidbody>().useGravity = false;
             }
-            Debug.Log(SandMoveFtSp);
 
             // プレイヤーの中砂が落ちきっていたら埋まる処理（徐々に埋まっていく感じに変更予定）
-            if (PSandEnpflg == true)
-            {
-                //FtCol.center = new Vector3(0.0f,0.25f,0.0f);
-                //FtCol.size = new Vector3(1.0f,0.5f,1.0f);
-            }
+
+            FtCol.center = new Vector3(B_Col_Center.x, B_Col_Center.y, B_Col_Center.z);
+            FtCol.size = new Vector3(B_Col_Size.x, B_Col_Size.y, B_Col_Size.z);
+
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        //流砂から離れるときに流砂の影響を消す
         if (other.gameObject.tag == "QuickSand_B")
         {
             this.GetComponent<Rigidbody>().useGravity = true;
