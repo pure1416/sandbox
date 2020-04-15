be0d626245cd09bebdba78fc5b2f5e5b3f14227a
Amalgam0616 <56029851+Amalgam0616@users.noreply.github.com>
2020-04-15 14:41:11 +0900

    ファイル整理とか

---
+++ Assets/Script/Data/WorldUnlock.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldUnlock : MonoBehaviour
+{
+    [Header("Flags")]
+    public bool UnlockFlg;    //アンロックフラグ
+    public bool ClearFlg;     //クリアフラグ
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockFlg = ClearFlg = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UnlockFlg)
+        {
+            if (ClearFlg)
+            {
+                //クリアしてたら緑にする
+                this.GetComponent<Image>().color = Color.green;
+            }
+            else
+            {
+                //アンロックだけなら青くする
+                this.GetComponent<Image>().color = Color.blue;
+            }
+        }
+        else
+        {
+            //アンロックされていない場合は赤くしてクリアフラグを切る
+            this.GetComponent<Image>().color = Color.red;
+            ClearFlg = false;
+        }
+    }
+
+    //UnlockフラグのSetter
+    public void SetUnlockFlg(bool bU)
+    {
+        UnlockFlg = bU;
+    }
+
+    //ClearフラグのSetter
+    public void SetClearFlg(bool bC)
+    {
+        ClearFlg = bC;
+    }
+}
