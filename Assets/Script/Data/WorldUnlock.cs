9660af65a3ead17a7e336803e6de4867e973fa37
hiromu <kh.534061@gmail.com>
2020-04-16 14:24:45 +0900

    ﾜｶﾗﾝ

---
+++ Assets/Script/Data/WorldUnlock.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WorldUnlock : MonoBehaviour
+{
+    [Header("Flags")]
+    public bool UnlockFlg;    //アンロックフラグ
+    public bool ClearFlg;     //クリアフラグ
+
+    [Header("Next Stages")]
+    public GameObject[] NextWorlds = new GameObject[4];      //次のステージ(0:上、1:右、2:下、3:左)
+
+    [Header("Scene")]
+    [SerializeField] private string GoScene;   //行先のシーン
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnlockFlg = ClearFlg = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UnlockFlg)
+        {
+            if (ClearFlg)
+            {
+                //クリアしてたら緑にする
+                this.GetComponent<Image>().color = Color.green;
+            }
+            else
+            {
+                //アンロックだけなら青くする
+                this.GetComponent<Image>().color = Color.blue;
+            }
+        }
+        else
+        {
+            //アンロックされていない場合は赤くしてクリアフラグを切る
+            this.GetComponent<Image>().color = Color.red;
+            ClearFlg = false;
+        }
+    }
+
+    //UnlockフラグのSetter
+    public void SetUnlockFlg(bool bU)
+    {
+        UnlockFlg = bU;
+    }
+
+    //ClearフラグのSetter
+    public void SetClearFlg(bool bC)
+    {
+        ClearFlg = bC;
+    }
+
+    //UnlockフラグのGetter
+    public bool GetUnlockFlg()
+    {
+        return UnlockFlg;
+    }
+
+    //ClearフラグのGetter
+    public bool GetClearFlg()
+    {
+        return ClearFlg;
+    }
+
+    //次のステージのUnlockFlg取得
+    public GameObject GetNextWorlds(int dir)
+    {
+        //0:上　1:右　2:下　3:左
+        //設定されてなかったら自分を返す
+        if (NextWorlds[dir] == null)
+        {
+            return this.gameObject;
+        }
+
+        return NextWorlds[dir]; ;
+    }
+
+    //シーン遷移
+    public void GoSceneChange()
+    {
+        SceneManager.LoadScene(GoScene);
+    }
+}
