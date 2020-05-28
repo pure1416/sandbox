f643d4d65e18912b1cbe45aecd3b1ac75b07e1d7
NEPIA0605 <ryty1021@gmail.com>
2020-05-29 00:26:49 +0900

    コミットバグ

---
+++ Assets/Script/StageRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageRotate : MonoBehaviour
+{
+    [Header("回転速度")]
+    public float RotSpd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0.0f, RotSpd, 0.0f);
+    }
+}
--- Assets/Script/player/ClearMotionManager.cs
+++ Assets/Script/player/ClearMotionManager.cs
@@ -6,62 +6,38 @@ public class ClearMotionManager : MonoBehaviour
 {
     private GameObject PlayerObg;
     private bool GameClearFlg;
-    private GameObject Rupee;
     float ClearModelAnimTime;
     Animator animator;
-    private Vector3 p0,p1, p2, p3;
-    float t;
 
-    bool testbejie;
+    bool RupeeAnimFlg;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        Rupee = GameObject.Find("rupee");
         PlayerObg = GameObject.Find("player");
         animator = GetComponent<Animator>();
         ClearModelAnimTime = 0.0f;
-        t = 0.0f;
-        testbejie = false;
+        RupeeAnimFlg = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            p0 = Rupee.transform.position;
-            p1 =(Rupee.transform.position - PlayerObg.transform.position) / 2;
-            p2 = PlayerObg.transform.position;
-
-
-
-
-
-            testbejie = true;
-        }
-        //Debug.Log("P0"+p0);
-        //Debug.Log("P1"+p1);
-        //Debug.Log("P2"+p2);
-        //Debug.Log("P3"+p3);
         GameClearFlg = PlayerObg.GetComponent<PlayerControler>().GetGameClearFlg();
-        if(GameClearFlg == true)
+        if (GameClearFlg == true)
         {
             animator.SetBool("clear", true);
             ClearModelAnimTime += Time.deltaTime;
         }
         if (ClearModelAnimTime >= 1.5f)
         {
-           
+            RupeeAnimFlg = true;
         }
+    }
 
-        if (testbejie == true)
-        {
-            t += Time.deltaTime / 3;
-            Vector3 Q0 = Vector3.Lerp(p0, p1, t);
-            Vector3 Q1 = Vector3.Lerp(p1, p2, t);
-            Vector3 Q2 = Vector3.Lerp(Q0, Q1, t);
-
-            Rupee.transform.position = Q2;    // 黒色の点
-        }
-     }
+    public bool GetRupeeAnimFlg()
+    {
+        return RupeeAnimFlg;
+    }
 }
