375c7d7707740db32083175154c5f33f346f5ef9
J <pure14_16@yahoo.co.jp>
2020-05-29 01:23:52 +0900

    フェード直した

--- Assets/Script/Title/TitleManager.cs
+++ Assets/Script/Title/TitleManager.cs
@@ -136,13 +136,15 @@ public class TitleManager : MonoBehaviour
     //ゲーム終了を押したとき
     public void PushExitButton()
     {
-
+        //フェード中入力できなくする処理
+        if ((FadeObj.GetComponent<FadeManager>().GetFadeInFlg() == false) && (!OptionObj.activeSelf))
+        {
 #if UNITY_EDITOR
 		UnityEditor.EditorApplication.isPlaying = false;
 #elif UNITY_WEBPLAYER
-		Application.OpenURL("http://www.yahoo.co.jp/");
 #else
-        Application.Quit();
+            Application.Quit();
 #endif
+        }
     }
 }
