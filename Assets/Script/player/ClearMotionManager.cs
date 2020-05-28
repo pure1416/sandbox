975d5e7a3ff596667b7a60461db17f2bd3c70fe8
J <pure14_16@yahoo.co.jp>
2020-05-28 15:40:43 +0900

    クリア演出追加

--- Assets/Script/player/ClearMotionManager.cs
+++ Assets/Script/player/ClearMotionManager.cs
@@ -6,38 +6,26 @@ public class ClearMotionManager : MonoBehaviour
 {
     private GameObject PlayerObg;
     private bool GameClearFlg;
-    private GameObject Rupee;
     float ClearModelAnimTime;
     Animator animator;
-    private Vector3 p0,p1, p2, p3;
+
     bool RupeeAnimFlg;
-    bool RupeePosSetFlg;
 
-    float t;
 
-    bool testbejie;
     // Start is called before the first frame update
     void Start()
     {
-        Rupee = GameObject.Find("rupee");
         PlayerObg = GameObject.Find("player");
         animator = GetComponent<Animator>();
         ClearModelAnimTime = 0.0f;
-        t = 0.0f;
-        testbejie = false;
         RupeeAnimFlg = false;
-        RupeePosSetFlg = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("P0"+p0);
-        Debug.Log("P1"+p1);
-        Debug.Log("P2"+p2);
-        
         GameClearFlg = PlayerObg.GetComponent<PlayerControler>().GetGameClearFlg();
-        if(GameClearFlg == true)
+        if (GameClearFlg == true)
         {
             animator.SetBool("clear", true);
             ClearModelAnimTime += Time.deltaTime;
@@ -45,34 +33,11 @@ public class ClearMotionManager : MonoBehaviour
         if (ClearModelAnimTime >= 1.5f)
         {
             RupeeAnimFlg = true;
-
         }
-        if (RupeeAnimFlg == true)
-        {
-            if (Rupee.transform.position.y <= 1.5f)
-            {
-                Rupee.transform.position.y += 0.1f;
-            }
-            if (RupeePosSetFlg == false)
-            {
-                if (Rupee.transform.position.y >= 1.5f)
-                {
-                    p0 = Rupee.transform.position;
-                    p1 = (Rupee.transform.position + PlayerObg.transform.position) / 2;
-                    p1.y += Rupee.transform.position.y + 5.0f;
-                    p2 = PlayerObg.transform.position;
-                    RupeePosSetFlg = true;
-                }
-            }
-            if (RupeePosSetFlg == true)
-            {
-                t += Time.deltaTime / 3;
-                Vector3 Q0 = Vector3.Lerp(p0, p1, t);
-                Vector3 Q1 = Vector3.Lerp(p1, p2, t);
-                Vector3 Q2 = Vector3.Lerp(Q0, Q1, t);
+    }
 
-                Rupee.transform.position = Q2;    // 黒色の点
-            }
-        }
-     }
+    public bool GetRupeeAnimFlg()
+    {
+        return RupeeAnimFlg;
+    }
 }
