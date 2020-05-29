8e1eccd7cb3911e313cefe2243e90e498eb9192f
Amalgam0616 <56029851+Amalgam0616@users.noreply.github.com>
2020-05-29 18:34:49 +0900

    フレーム管理

---
+++ Assets/Script/Title/FrameSetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //目標FPS設定
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
